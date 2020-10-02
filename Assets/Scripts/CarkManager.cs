68339af586940a1e70d7683253235774666204d3
ismailonur <ismailonur15@gmail.com>
2020-10-02 14:11:24 +0300

    first commit ve Menu level tasarımı yapıldı

---
+++ Assets/Scripts/CarkManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarkManager : MonoBehaviour
+{
+    public int donusHizi;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * donusHizi * Time.deltaTime);
+    }
+}
