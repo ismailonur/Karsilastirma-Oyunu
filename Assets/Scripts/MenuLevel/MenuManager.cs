ce0e48caf950b6bd07c0e7bfa4881d9bf1ebb0c8
ismailonur <ismailonur15@gmail.com>
2020-10-02 14:50:09 +0300

    MenuLevel sahnesi bitti

---
+++ Assets/Scripts/MenuLevel/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuManager : MonoBehaviour
+{
+    public Transform kafa;
+    public Transform startButon;
+
+    void Start()
+    {
+        kafa.transform.GetComponent<RectTransform>().DOLocalMoveY(150f, 1f).SetEase(Ease.OutBack);
+        startButon.transform.GetComponent<RectTransform>().DOLocalMoveY(-200f, 1f).SetEase(Ease.OutBack);
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void OyunaBasla()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+}
