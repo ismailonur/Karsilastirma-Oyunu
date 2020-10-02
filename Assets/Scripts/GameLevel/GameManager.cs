d48ac22da95d3011dcbd5a64b1a0b16fa6dc2372
ismailonur <ismailonur15@gmail.com>
2020-10-02 18:01:22 +0300

    GameLevel sahnesindeki UI lar hazırlandı

---
+++ Assets/Scripts/GameLevel/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ustPanel;
+    public GameObject altYazi;
+    public GameObject ustDikdortgen;
+    public GameObject altDikdortgen;
+
+    void Start()
+    {
+        SahnedekileriGetir();
+    }
+
+    void SahnedekileriGetir()
+    {
+        ustPanel.GetComponent<CanvasGroup>().DOFade(1, 1f);
+        altYazi.GetComponent<CanvasGroup>().DOFade(1, 1f);
+
+        ustDikdortgen.transform.GetComponent<RectTransform>().DOLocalMoveX(0, 1f).SetEase(Ease.OutBack);
+        altDikdortgen.transform.GetComponent<RectTransform>().DOLocalMoveX(0, 1f).SetEase(Ease.OutBack);
+    }
+
+    public void OyunaBasla()
+    {
+        Debug.Log("Başladı");
+    }
+}
---
+++ Assets/Scripts/GameLevel/GeriSayimManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GeriSayimManager : MonoBehaviour
+{
+    public GameObject geriSayim;
+    public Text geriSayimText;
+
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = Object.FindObjectOfType<GameManager>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(GeriyeSayRoutine());
+    }
+
+    IEnumerator GeriyeSayRoutine()
+    {
+        geriSayimText.text = "3";
+        yield return new WaitForSeconds(.5f);
+
+        geriSayim.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(.5f);
+        geriSayim.GetComponent<RectTransform>().DOScale(0, 0.5f).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(.5f);
+
+        geriSayimText.text = "2";
+        yield return new WaitForSeconds(.5f);
+
+        geriSayim.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(.5f);
+        geriSayim.GetComponent<RectTransform>().DOScale(0, 0.5f).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(.5f);
+
+        geriSayimText.text = "1";
+        yield return new WaitForSeconds(.5f);
+
+        geriSayim.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(.5f);
+        geriSayim.GetComponent<RectTransform>().DOScale(0, 0.5f).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(.5f);
+
+        StopAllCoroutines();
+
+        gameManager.OyunaBasla();
+    }
+}
